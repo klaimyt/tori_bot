c81d9f7a4a741e0f20c95b7c51874722e8ccf7c4
Klaimyt <vano-violinist@yandex.ru>
2021-10-05 23:07:55 +0300

    Add scraping function. The function takes url and returns products array. Not done yet

---
+++ dataScraper.js
@@ -0,0 +1,28 @@
+﻿const axios = require('axios')
+const cheerio = require('cheerio')
+
+async function htmlScrapingFrom(url) {
+  const htmlFile = await axios.get(url).then(response => {
+    return response.data
+  })
+  
+  let $ = cheerio.load(htmlFile)
+
+  const productsHTML = Array.from($('.list_mode_thumb > a'))
+
+  const products = productsHTML.map(product => {
+    $ = cheerio.load(product)
+    const priceString = $('.list_price.ineuros').text()
+    // Get actual number from string. Set 0 if string empty
+    const price = priceString ? parseInt(priceString.replace(/[€ ]/g, '')) : 0
+    // Product object
+    return {
+      name: $('.li-title').text(),
+      id: product.attribs.id.replace('item_', ''),
+      link: product.attribs.href,
+      price: price
+    }
+  })
+}
+
+module.exports = htmlScrapingFrom
